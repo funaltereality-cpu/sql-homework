dc43f5812102645937ec90eb3f9e0a2041418a12
Valentina Kudelko <funaltereality@gmail.com>
2025-12-21 00:23:50 +0300

    Edited

--- final_practice.sql
+++ final_practice.sql
@@ -1013,13 +1013,13 @@ CREATE TABLE IF NOT EXISTS review (
    rating INT NOT NULL,
    client_id INT,
    trip_id INT,
-   date DATE NOT NULL,
+   review_date DATE NOT NULL,
    FOREIGN KEY(client_id) REFERENCES client(client_id),
    FOREIGN KEY(trip_id) REFERENCES trip(trip_id),
    UNIQUE(client_id, trip_id),
    check(description <> ' '));
 
-  INSERT INTO review (description, rating, client_id, trip_id, date)
+  INSERT INTO review (description, rating, client_id, trip_id, review_date)
   VALUES ('Awesome trip', 5, 9, 3, '2025-07-13'),
   ('The best thing to do during summer!', 5, 12, 2, '2025-10-02'),
   ('The guide was very knowledgeable', 5, 12, 5, '2025-10-26')
@@ -1040,14 +1040,14 @@ begin
     from trip 
     where trip_id =new.trip_id;
     
-    if new.date < trip_end then
+    if new.review_date < trip_end then
 	SIGNAL SQLSTATE '45000' 
     SET MESSAGE_TEXT = 'Cannot add the review before the trip ends';
 		 end if; 
          end$$
 delimiter ;
 
-insert into review (description, rating, date, trip_id, client_id)
+insert into review (description, rating, review_date, trip_id, client_id)
 VALUES('Did not like it. It was rubbish. Never again', 1, '2025-01-30', 4, 12);
 
 -- оставлено для проверки работает ли триггер: delete from review where review_id = 4;
